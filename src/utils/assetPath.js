30f551c89698927ac8c50a8aa64b2e159016f317
Pratik Raut <raut70856@gmail.com>
2026-03-12 22:50:34 +0530

    fix svg assets

--- src/utils/assetPath.js
+++ src/utils/assetPath.js
@@ -1,4 +1,3 @@
-// Utility function to resolve asset paths with PUBLIC_URL prefix
 export const getAssetPath = (file) => {
-  return `${process.env.PUBLIC_URL}/assets/${file}`;
+  return process.env.PUBLIC_URL + "/assets/" + file;
 };
